84baae899a252a81ee49e43f200e5334d1d456fe
Adam Driscoll <adam.r.driscoll@gmail.com>
2016-06-06 09:41:12 -0500

    Fixing dropAndCreate.bat

--- IntegrationPlatform/Setup/Installation/Database/dropAndCreate.bat
+++ IntegrationPlatform/Setup/Installation/Database/dropAndCreate.bat
@@ -2,7 +2,7 @@ if "%1"=="/?" goto USAGE
     if "%1"=="" goto USAGE
      
     cd "C:\Program Files (x86)\Microsoft SQL Server\110\Tools\Binn"
-    osql -E -i C:\Users\adriscol\Source\Repos\TfsIntegrationPlatform\IntegrationPlatform\Setup\Installation\Database\Tfs_IntegrationPlatform.sql -S %1 -n
+    osql -E -i Tfs_IntegrationPlatform.sql -S %1 -n
      
     goto END
      
